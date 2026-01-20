be9b031a9426530ed5695ed87455c9f26211473c
faten <fatensafad@gmail.com>
2026-01-20 14:00:58 +0300

    Initial commit

---
+++ src/sections/IdeaGallery/IdeaGallery.jsx
@@ -0,0 +1,11 @@
+import styles from './IdeaGallery.module.css'
+
+function IdeaGallery() {
+  return (
+    <div className={styles.container}>
+      <p>Idea Gallery Component</p>
+    </div>
+  )
+}
+
+export default IdeaGallery
---
+++ src/sections/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './NavBar.module.css';
+
+export default function NavBar() {
+    return (
+        <div className={styles.container}>
+            <p>Placeholder for NavBar Component</p>
+            
+        </div>
+    );
+}
