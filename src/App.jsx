d0f8218d880e58b84ddf5612dd669c647878dbf6
atheer maklad <atheermk21@gmail.com>
2026-01-21 23:20:49 +0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-
-
-function App() {
-
-
-  return (
-    <>
-
-    </>
-  )
+import React from "react";
+import IdeaForm from "./sections/IdeaForm/IdeaForm";
+function App(){
+return(
+<>
+<IdeaForm/>
+</>
+);
 }
 
-export default App
+export default App;
--- src/sections/IdeaForm/IdeaForm.jsx
+++ src/sections/IdeaForm/IdeaForm.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './IdeaForm.module.css';
-
 export default function IdeaForm() {
     return (
         <div className={styles.container}>
-            <p>Placeholder for IdeaForm Component</p>
+            <h1>Idea Board</h1>
+            <p>Capture, organize,and track your creative ideas in one place</p>
+            <div className={styles.card}>
+                <h2>Add New Idea </h2>
+                {/* <h3>Idea Title</h3> */}
+                <form className={styles.formGroup}>
+                    <label>Idea Title</label>
+                    <input type="text" placeholder='Enter your idea..' />
+                    <span className={styles.inform}>100 charactres remaining</span>
+                    <label>category (Optional)</label>
+                  <select >
+                    <option> General</option>
+                  </select>
+           <button className={styles.actionBtn}>Add idea</button>
+                </form>
+            </div>
         </div>
     );
 }
