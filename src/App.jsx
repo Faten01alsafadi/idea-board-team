672a729f44fef5d5e9514ff396ef89ff40055b58
atheer maklad <atheermk21@gmail.com>
2026-01-29 16:28:30 +0300

    build hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
 import React from "react";
-import IdeaForm from "./sections/IdeaForm/IdeaForm";
+import Hero from './sections/Component/Hero/Hero'
+// import IdeaForm from "./sections/IdeaForm/IdeaForm";
 function App(){
 return(
-<>
-<IdeaForm/>
-</>
+    <div>
+    <Hero 
+    title="Turn Your Ideas Into"
+    highlight="Reality"
+    description="Capture, organize, and bring your creative vision to life ...one idea at a time "
+    backgroundImage="/assets/images/photo_٢٠٢٦-٠١-٢٩_١٤-١٩-٤٦.jpg"
+/>
+{/* <IdeaForm/> */}
+</div>
 );
-}
+};
 export default App;
+
