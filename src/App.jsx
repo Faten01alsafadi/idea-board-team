ceb98c7e629b0d9c48de1a69850b963e97466713
faten <fatensafad@gmail.com>
2026-02-16 13:33:35 +0300

    Refactor layout and update routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import SignUp from "./pages/SignUp/SignUp"
+import SignIn from "./pages/SignIn/SignIn"
+import Landing from "./pages/Landing/Landing"
 
 
 
@@ -8,7 +12,13 @@ function App() {
 
   return (
     <>
-
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Landing() {
+  return (
+    // استدعو اقسامكن هون بدل بال
+    // App
+    // بالترتيب الصحيح
+    <div>
+        {/* مثال */}
+      {/* <Nav/> */}
+      {/* <Hero/> */}
+    </div>
+  )
+}
+
+export default Landing
